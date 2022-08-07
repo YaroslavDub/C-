9496af4fbb70fb95ae2896b186386c9c29a99b8c
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-07 20:49:06 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first > second) {
+    Console.WriteLine("Максимальное число: " + first);
+    Console.WriteLine("Минимальное число: " + second);
+}
+else if (second > first) {
+    Console.WriteLine("Максимальное число: " + second);
+    Console.WriteLine("Минимальное число: " + first);
+}
+else {
+    Console.WriteLine("Оба числа одинаковые");
+}
